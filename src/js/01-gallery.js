56960b51caae0bd1da6e7f69f02baa7f8daffb18
Alex Koniushenko <alex.koniushenko@gmail.com>
2022-11-03 19:15:35 +0200

    1 task completed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,19 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+let markUp = '';
+for (const galleryItem of galleryItems) {
+    markUp += `
+    <a class="gallery__item" href="${galleryItem.original}">
+    <img class="gallery__image" src="${galleryItem.preview}" alt="Image description" /> 
+    </a>`; 
+}
+
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.innerHTML = markUp;
+const lightbox = new SimpleLightbox('.gallery a', 
+{captions: true, captionsData: 'alt', captionDelay: 250});
 
 console.log(galleryItems);
