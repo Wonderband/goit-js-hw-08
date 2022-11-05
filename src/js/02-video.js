d5b3b06aecdf974c598b38bf3eeed2a5eed76fa6
Alex Koniushenko <alex.koniushenko@gmail.com>
2022-11-05 23:50:55 +0200

    Almost done - -lodash to add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+// import Vimeo from '@vimeo/player';
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+// localStorage.setItem("videoplayer-current-time", "0");
+
+
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
+player.on('timeupdate', data => {
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+   console.log(data.seconds);
+    // data is an object containing properties specific to that event
+});
+
+function playFromStop() {
+    const timeSaved = localStorage.getItem("videoplayer-current-time") || "0";
+    // console.log(timeSaved);
+    player.setCurrentTime(timeSaved);
+    // console.log(player.getCurrentTime());    
+}
+window.addEventListener('DOMContentLoaded', playFromStop);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
 
+
+const onInputHandle = function(event) {
+    try {
+        const { elements: {email, message}} = event.currentTarget;  
+    localStorage.setItem("feedback-form-state", 
+    JSON.stringify({email: email.value, message: message.value}));
+    // console.log(localStorage.getItem("feedback-form-state"));
+    } catch(error) {
+        console.error("Get state error: ", error.message);
+    }
+}
+
+const loadFromStorage = function(event) {
+    try {
+        const dataStored = JSON.parse(localStorage.getItem("feedback-form-state")) ||
+        {"email": "", "message": ""};
+        emailEl.value = dataStored.email || "";
+        textAreaEl.value = dataStored.message || "";
+    } catch(error) {
+        console.error("Get state error: ", error.message);
+    }
+}
+
+const totalReset = function(event) {
+    event.preventDefault();   
+    console.log(localStorage.getItem("feedback-form-state") || "Local Storage is empty! ");       
+    emailEl.value = '';
+    textAreaEl.value = '';
+    localStorage.clear();
+}
+
+const formEl = document.querySelector(".feedback-form");
+const emailEl = document.querySelector("input");
+const textAreaEl = document.querySelector("textarea");
+
+
+window.addEventListener('DOMContentLoaded', loadFromStorage);
+formEl.addEventListener('input', onInputHandle);
+formEl.addEventListener('submit', totalReset);
+
